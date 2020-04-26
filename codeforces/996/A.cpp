62c4383e107c3d4a9b52c83e4ee8fb91f246b393
Suveen Kumar <suveenkumar1997@gmail.com>
2020-04-26 22:16:00 +0530

    Add solution for problem `A - Hit the Lottery` Link: https://codeforces.com/contest/996/problem/A Tags: dp, greedy, *800 Ref: https://codeforces.com/contest/996/submission/78212160

--- codeforces/996/A.cpp
+++ codeforces/996/A.cpp
@@ -2,36 +2,34 @@
 #define ll long long
 #define MOD 1000000007 
 using namespace std;
-void solve(){
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     ll n;
     cin>>n;
-    ll cnt=0;
+    int deno[5]={1,5,10,20,100};
+    ll count=0;
     while(n>0){
         if(n>=100){
-            cnt++;
+            count++;
             n-=100;
         }
-        else if(n>=20){
-            cnt++;
+        else if(n>=20 && n<100){
+            count++;
             n-=20;
         }
-        else if(n>=10){
-            cnt++;
+        else if(n>=10 && n<20){
+            count++;
             n-=10;
         }
-        else if(n>=5){
-            cnt++;
+        else if(n>=5 && n<10){
+            count++;
             n-=5;
         }
         else{
-            cnt++;
-            n--;
+           count++;
+           n--;
         }
     }
-    cout<<cnt<<endl;
-}
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    solve();
+    cout<<count<<endl;
     return 0;
 }
