ad7f50f1eb162a6af428b613a5c1b8e8af894578
Suveen Kumar <suveenkumar1997@gmail.com>
2020-06-23 19:47:00 +0530

    Add solution for problem `A - FashionabLee` Link: https://codeforces.com/contest/1369/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1369/submission/84764948

---
+++ codeforces/1369/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1000000007 
+using namespace std;
+void solve(){
+    ll n;
+    cin>>n;
+    if(n%4==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
