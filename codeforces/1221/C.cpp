8720454f21e7c0fb9b7885913504534f61f99a42
Suveen Kumar <suveenkumar1997@gmail.com>
2020-07-22 13:51:00 +0530

    Add solution for problem `C - Perfect Team` Link: https://codeforces.com/contest/1221/problem/C Tags: binary search, math, *1200 Ref: https://codeforces.com/contest/1221/submission/87650951

--- codeforces/1221/C.cpp
+++ codeforces/1221/C.cpp
@@ -7,18 +7,18 @@ void solve(){
     cin>>c>>m>>x;
     ll s=1;
     ll e=min(c,m);
-    ll ans=0;
+    ll res=0;
     while(s<=e){
         ll mid=(s+e)/2;
         if(mid<=(x+c+m-2*mid)){
-            ans=mid;
+            res=mid;
             s=mid+1;
         }
         else{
             e=mid-1;
         }
     }
-    cout<<ans<<endl;
+    cout<<res<<endl;
 }
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
