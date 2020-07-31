3b5412f3bc9a40e7d05358a4cefa68a0f870f8ec
Suveen Kumar <suveenkumar1997@gmail.com>
2020-07-31 14:11:00 +0530

    Add solution for problem `B - Physics Practical` Link: https://codeforces.com/contest/253/problem/B Tags: binary search, dp, sortings, two pointers, *1400 Ref: https://codeforces.com/contest/253/submission/88573964

--- codeforces/253/B.cpp
+++ codeforces/253/B.cpp
@@ -1,32 +1,33 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define MOD 1000000007 
+#define ll long long int
 using namespace std;
-void solve(){
-   ll n;
-   cin>>n;
-   ll a[n];
-   for(ll i=0;i<n;i++){
-      cin>>a[i];
-   }
-   sort(a,a+n);
-   if(((double)a[n-1]/a[0])<=2){
-        cout<<0<<endl;     
-   }
-   else{
-       ll ans=MOD;
-       for(ll i=-1;i<n;i++){
-          ll x=upper_bound(a,a+n,2*(a[i+1]))-a;
-          ans=min(ans,i+1+n-x);
-       }
-       cout<<ans<<endl;
-   }
-   
+void hehe()
+{
+    freopen("input.txt", "rt", stdin);
+    freopen("output.txt", "wt", stdout);
+    int n=1;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    int ans = n;
+    for (int i = 0; i < n; i++)
+    {
+        auto itr = lower_bound(arr.begin(), arr.end(), 2 * arr[i] + 1);
+        ans = min(ans, arr.end() - itr + i);
+    }
+    cout << ans << endl;
 }
-int main() {
-    freopen("input.txt","rt", stdin);
-    freopen("output.txt","wt", stdout);
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    solve();
+int main()
+{
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        hehe();
+    }
     return 0;
 }
