a44cf20c8ad2f3544fa4df047dc1bd500beb2bcb
Suveen Kumar <suveenkumar1997@gmail.com>
2020-08-03 18:09:00 +0530

    Add solution for problem `E - Tetrahedron` Link: https://codeforces.com/contest/166/problem/E Tags: dp, math, matrices, *1500 Ref: https://codeforces.com/contest/166/submission/88798748

---
+++ codeforces/166/E.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1000000007 
+using namespace std;
+void solve(){
+    ll n;
+    cin>>n;
+    vector<ll> dp(n+1,0);
+    for(ll i=2;i<=n;i++){
+        if(i%2==0)dp[i]=(dp[i-1]*3+3)%MOD;
+        else dp[i]=(dp[i-1]*3-3)%MOD;
+    }
+    cout<<dp[n]<<endl;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w", stdout);
+#endif
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
