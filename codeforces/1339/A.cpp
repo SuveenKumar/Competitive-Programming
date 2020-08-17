71c85880fc1cd076e3df8a6396a97767d7314064
Suveen Kumar <suveenkumar1997@gmail.com>
2020-08-17 19:28:00 +0530

    Add solution for problem `A - Filling Diamonds` Link: https://codeforces.com/contest/1339/problem/A Tags: brute force, dp, implementation, math, *900 Ref: https://codeforces.com/contest/1339/submission/90234713

---
+++ codeforces/1339/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1000000007 
+using namespace std;
+void solve(){
+   ll n;
+   cin>>n;
+   cout<<n<<endl;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w", stdout);
+#endif
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--)solve();
+    return 0;
+}
