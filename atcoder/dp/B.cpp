89d67a7e92aea6880c9665a84d49595b3f8e54e5
Suveen Kumar <suveenkumar1997@gmail.com>
2020-08-19 09:29:00 +0530

    Add solution for problem `B - Frog 2` Link: https://atcoder.jp/contests/dp/tasks/dp_b Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/16034586

--- atcoder/dp/B.cpp
+++ atcoder/dp/B.cpp
@@ -1,50 +1,37 @@
 #include <bits/stdc++.h>
-using namespace std;
 #define ll long long
-#define endl ("\n")
-#define pi (3.141592653589)
-#define mod 1e9+7
-#define INF 1e9+9
-#define float double
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define so sizeof 
-#define pii pair<ll,ll>
-#define all(c) c.begin(), c.end()
-#define min3(a, b, c) min(c, min(a, b))
-#define min4(a, b, c, d) min(d, min(c, min(a, b)))
-#define max3(a, b, c) max(c, max(a, b))
-#define max4(a, b, c, d) max(d, max(c, max(a, b)))
-#define repe(i,a) for(auto i:a)
-#define rep(i,a,b) for(ll i=a;i<=b;i++)
-#define rrep(i,a,b) for(ll i=b;i>=a;i--)
-#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define MOD 1000000007 
+using namespace std;
 void solve(){
-  ll n,k;
-  cin>>n>>k;
-  ll a[n+1];
-  rep(i,1,n)cin>>a[i];
-  ll dp[n+1];
-  dp[0]=dp[1]=0;
-  rep(i,2,n){
-    ll res=INF;
-    rep(j,1,k){
-        if(i-j>=1){
-           res=min((abs(a[i]-a[i-j])+dp[i-j]),res);
-        }
-    }
-    dp[i]=res;
-  }
-  cout<<dp[n]<<endl;
+   ll n,k;
+   cin>>n>>k;
+   ll a[n+1];
+   for(ll i=1;i<=n;i++){
+      cin>>a[i];
+   }
+   ll dp[n+2];
+   dp[1]=0;
+   for(ll i=2;i<=n;i++){
+       ll minm=INT_MAX;
+       for(ll j=1;j<=k;j++){
+          if(i-j>=1){
+             ll op=dp[i-j]+abs(a[i]-a[i-j]);
+             minm=min(minm,op);
+           }
+           else{
+            break;
+           }
+       }
+       dp[i]=minm;     }
+   cout<<dp[n]<<endl;
+
 }
 int main() {
 #ifndef ONLINE_JUDGE
     freopen("input.txt","r", stdin);
     freopen("output.txt","w", stdout);
 #endif
-    fast
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     solve();
     return 0;
 }
