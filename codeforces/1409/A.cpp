a38f6d5375979b0eab03830494e13f6008040599
Suveen Kumar <suveenkumar1997@gmail.com>
2020-09-04 20:10:00 +0530

    Add solution for problem `A - Yet Another Two Integers Problem` Link: https://codeforces.com/contest/1409/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1409/submission/91807946

---
+++ codeforces/1409/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1000000007
+#define INF 1000000009
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define so sizeof 
+#define pii pair<ll,ll>
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define max3(a, b, c) max(c, max(a, b))
+#define max4(a, b, c, d) max(d, max(c, max(a, b)))
+#define repe(i,a) for(auto i:a)
+#define st string
+#define rep(i,a,b) for(ll i=a;i<=b;i++)
+#define rrep(i,a,b) for(ll i=b;i>=a;i--)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr); 
+void solve(){
+  ll a,b;
+  cin>>a>>b;
+  ll res=abs(b-a);
+  long double ans=res/10.0;
+  cout<<(ll)ceil(ans)<<endl;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w", stdout);
+#endif
+    fast
+    ll t;
+    cin>>t;
+    while(t--)
+    solve();
+    return 0;
+}
