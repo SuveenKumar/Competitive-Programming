2d7eaf004de5f0d5ba96610ecea8f4a85ecd09a9
Suveen Kumar <suveenkumar1997@gmail.com>
2020-09-25 21:51:00 +0530

    Add solution for problem `I - Coins` Link: https://atcoder.jp/contests/dp/tasks/dp_i Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/17008365

---
+++ atcoder/dp/I.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1000000007
+#define INF 1000000009
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define so sizeof 
+#define pii pair<ll,ll>
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define max3(a, b, c) max(c, max(a, b))
+#define max4(a, b, c, d) max(d, max(c, max(a, b)))
+#define repe(i,a) for(auto i:a)
+#define st string
+#define rep(i,a,b) for(ll i=a;i<=b;i++)
+#define rrep(i,a,b) for(ll i=b;i>=a;i--)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr); 
+void solve(){
+   int n;
+   cin>>n;
+   float a[n];
+   rep(i,0,n-1){
+    cin>>a[i];
+   }
+   float dp[n][n+1];
+   rep(i,0,n-1){
+    rep(j,0,n){
+      dp[i][j]=0;
+    }
+   }
+   dp[0][0]=1-a[0];
+   dp[0][1]=a[0];
+   rep(i,1,n-1){
+       rep(j,0,i+1){
+           dp[i][j]+=dp[i-1][j]*(1-a[i]);
+           if(j>0)
+            dp[i][j]+=dp[i-1][j-1]*a[i];
+       }
+   }
+   float ans=0;
+   rep(i,n/2+1,n){
+     ans+=dp[n-1][i];
+   }
+   cout<<setprecision(9);
+  cout<<fixed;
+   cout<<ans<<endl;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w", stdout);
+#endif
+    fast
+    // ll t;
+    // cin>>t;
+    // while(t--)
+    solve();
+    return 0;
+}
