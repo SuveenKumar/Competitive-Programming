186f32972d174f239220c591be280319555e32d9
Suveen Kumar <suveenkumar1997@gmail.com>
2020-11-01 16:24:00 +0530

    Add solution for problem `B - Maximum Product` Link: https://codeforces.com/contest/1406/problem/B Tags: brute force, dp, greedy, implementation, sortings, *1200 Ref: https://codeforces.com/contest/1406/submission/97302363

---
+++ codeforces/1406/B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1000000007
+#define INF 1000000009
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define so sizeof 
+#define pii pair<ll,ll>
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define max3(a, b, c) max(c, max(a, b))
+#define max4(a, b, c, d) max(d, max(c, max(a, b)))
+#define minheap priority_queue<int,vector<int>,greater<int>>
+#define maxheap priority_queue<int>
+#define repe(i,a) for(auto i:a)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define rrep(i,a,b) for(ll i=b-1;i>=a;i--)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr); 
+void solve(){
+  ll n;
+  cin>>n;
+  ll a[n];
+  rep(i,0,n){
+    cin>>a[i];
+  }
+  sort(a,a+n);
+  ll op1=a[n-1]*a[n-2]*a[n-3]*a[n-4]*a[n-5];
+  ll op2=a[0]*a[1]*a[n-1]*a[n-2]*a[n-3];
+  ll op3=a[0]*a[1]*a[2]*a[3]*a[n-1];
+  cout<<max3(op1,op2,op3)<<endl;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+      freopen("input.txt","r", stdin);
+      freopen("output.txt","w", stdout);
+  #endif
+      fast
+      ll t;
+      cin>>t;
+      while(t--)
+        solve();
+      return 0;
+  }
