4ff92dcd64433da5072967756ab8281af66bf4fd
Suveen Kumar <suveenkumar1997@gmail.com>
2020-11-15 18:05:00 +0530

    Add solution for problem `C - Travel` Link: https://atcoder.jp/contests/abc183/tasks/abc183_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc183/submissions/18136908

---
+++ atcoder/abc183/C.cpp
@@ -0,0 +1,66 @@
+  #include <bits/stdc++.h>
+  using namespace std;
+  #define ll long long
+  #define endl ("\n")
+  #define pi (3.141592653589)
+  #define mod 1000000007
+  #define INF 1000000009
+  #define float double
+  #define pb push_back
+  #define mp make_pair
+  #define ff first
+  #define ss second
+  #define so sizeof 
+  #define pii pair<ll,ll>
+  #define all(c) c.begin(), c.end()
+  #define min3(a, b, c) min(c, min(a, b))
+  #define min4(a, b, c, d) min(d, min(c, min(a, b)))
+  #define max3(a, b, c) max(c, max(a, b))
+  #define max4(a, b, c, d) max(d, max(c, max(a, b)))
+  #define minheap priority_queue<int,vector<int>,greater<int>>
+  #define maxheap priority_queue<int>
+  #define repe(i,a) for(auto i:a)
+  #define rep(i,a,b) for(ll i=a;i<b;i++)
+  #define rrep(i,a,b) for(ll i=b-1;i>=a;i--)
+  #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr); 
+  void solve(){
+    ll n,s;
+    cin>>n>>s;
+    ll a[n+1][n+1];
+    rep(i,1,n+1){
+      rep(j,1,n+1){
+        cin>>a[i][j];
+      }
+    }
+    vector<ll> v;
+    rep(i,2,n+1){
+      v.pb(i);
+    }
+    ll cnt=0;
+    do { 
+        ll sum=0;
+        sum+=a[1][v[0]];
+     //   cout<<sum<<endl;
+        sum+=a[v[v.size()-1]][1];
+        rep(i,1,v.size()){
+          sum+=a[v[i-1]][v[i]];
+        }
+     //   cout<<sum<<endl;
+        if(sum==s){
+          cnt++;
+        } 
+    } while (next_permutation(v.begin(), v.end()));
+  cout<<cnt<<endl;
+  }
+  int main() {
+        #ifndef ONLINE_JUDGE
+        freopen("input.txt","r", stdin);
+        freopen("output.txt","w", stdout);
+    #endif
+        fast
+        // ll t;
+        // cin>>t;
+        // while(t--)
+          solve();
+        return 0;
+    }
