53f45723f2982001c7d4680342c171216c8c55df
Suveen Kumar <suveenkumar1997@gmail.com>
2020-11-18 16:03:00 +0530

    Add solution for problem `A - Filling Shapes` Link: https://codeforces.com/contest/1182/problem/A Tags: dp, math, *1000 Ref: https://codeforces.com/contest/1182/submission/98799605

---
+++ codeforces/1182/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pi (3.141592653589)
+#define mod 1000000007
+#define INF 1000000009
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define so sizeof 
+#define pii pair<ll,ll>
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define max3(a, b, c) max(c, max(a, b))
+#define max4(a, b, c, d) max(d, max(c, max(a, b)))
+#define repe(i,a) for(auto i:a)
+#define st string
+#define rep(i,a,b) for( i=a;i<=b;i++)
+#define rrep(i,a,b) for(ll i=b;i>=a;i--)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr); 
+void solve(){
+   ll n;
+   cin>>n;
+   ll ans=1;
+    
+   if(n%2!=0)cout<<0<<endl;
+   else{
+     cout<<(ll)pow(2,n/2)<<endl;
+     }
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w", stdout);
+#endif
+    fast
+    solve();
+    return 0;
+}
