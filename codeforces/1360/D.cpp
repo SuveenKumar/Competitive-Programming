cce0510dfab29f980cb175b5749d4835062b89eb
Suveen Kumar <suveenkumar1997@gmail.com>
2020-12-20 13:37:00 +0530

    Add solution for problem `D - Buying Shovels` Link: https://codeforces.com/contest/1360/problem/D Tags: math, number theory, *1300 Ref: https://codeforces.com/contest/1360/submission/101829570

---
+++ codeforces/1360/D.cpp
@@ -0,0 +1,56 @@
+  #include <bits/stdc++.h>
+  using namespace std;
+  #define ll long long
+  #define endl ("\n")
+  #define pi (3.141592653589)
+  #define mod 1000000007
+  #define INF 1000000009
+  #define float double
+  #define pb push_back
+  #define mp make_pair
+  #define ff first
+  #define ss second
+  #define so sizeof 
+  #define pii pair<ll,ll>
+  #define all(c) c.begin(), c.end()
+  #define min3(a, b, c) min(c, min(a, b))
+  #define min4(a, b, c, d) min(d, min(c, min(a, b)))
+  #define max3(a, b, c) max(c, max(a, b))
+  #define max4(a, b, c, d) max(d, max(c, max(a, b)))
+  #define minheap priority_queue<int,vector<int>,greater<int>>
+  #define maxheap priority_queue<int>
+  #define repe(i,a) for(auto i:a)
+  #define rep(i,a,b) for(ll i=a;i<b;i++)
+  #define rrep(i,a,b) for(ll i=b-1;i>=a;i--)
+  #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+  void solve(){
+    ll n,k;
+    cin>>n>>k;
+    if(n<=k){
+      cout<<1<<endl;
+    }
+    else{
+      ll ans=INF;
+      for(ll i=1;i*i<=n;i++){
+         if(n%i==0 && i<=k){
+             ans=min(ans,n/i);
+             if((n/i)<=k){
+                 ans=min(ans,n/(n/i));
+             }
+         }
+      }
+      cout<<ans<<endl;
+    }
+  }
+  int main() {
+  #ifndef ONLINE_JUDGE
+          freopen("input.txt","r", stdin);
+          freopen("output.txt","w", stdout);
+  #endif
+          fast
+          ll t;
+          cin>>t;
+          while(t--)
+            solve();
+          return 0;
+      }
