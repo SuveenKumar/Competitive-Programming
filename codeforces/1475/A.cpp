6d199ddbe965821d10a7865deb1c406260f22c7d
Suveen Kumar <suveenkumar1997@gmail.com>
2021-01-25 20:34:00 +0530

    Add solution for problem `A - Odd Divisor` Link: https://codeforces.com/contest/1475/problem/A Tags: math, number theory, *900 Ref: https://codeforces.com/contest/1475/submission/105313428

---
+++ codeforces/1475/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1000000007
+#define INF 1000000009
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define so sizeof 
+#define pii pair<ll,ll>
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define max3(a, b, c) max(c, max(a, b))
+#define max4(a, b, c, d) max(d, max(c, max(a, b)))
+#define minheap priority_queue<int,vector<int>,greater<int>>
+#define maxheap priority_queue<int>
+#define repe(i,a) for(auto i:a)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define rrep(i,a,b) for(ll i=b-1;i>=a;i--)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+void solve(){
+   ll n;
+   cin>>n;
+   while(n%2==0){
+      n/=2;
+   }
+   if(n==1){
+    cout<<"NO"<<endl;
+   }
+   else{
+    cout<<"YES"<<endl;
+   }
+}
+int main() {
+#ifndef ONLINE_JUDGE
+       freopen("input.txt","r", stdin);
+       freopen("output.txt","w", stdout);
+#endif
+      fast
+      ll t;
+      cin>>t;
+      while(t--)
+        solve();
+      return 0;
+}
